21805f081547e661f5fc709e8abc27e6d3ed5aaf
Build with Muzamal <buildwithmuzamal@gmail.com>
2026-09-03 14:24:31 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.reveal').forEach((element) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.animationPlayState = 'running';
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.12 });
+  observer.observe(element);
+});
